9ec586a64f6947bafce8d707c6c21caddc79f428
Andrei <azayats@xbsoftware.by>
2020-05-19 10:51:53 +0300

    fix port

--- index.js
+++ index.js
@@ -1,29 +1,28 @@
-var app = require('express')();
-var http = require('http').createServer(app);
-var io = require('socket.io')(http);
+const app = require("express")();
+app.set("port", process.env.PORT || 3000);
+const server = app.listen(app.get("port"), () => {
+	console.log("listening on port ", server.address().port);
+});
+const io = require("socket.io")(server);
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
+app.get("/", (req, res) => {
+	res.sendFile(`${__dirname}/index.html`);
 });
 
-app.get('/style.css', (req, res) => {
-    res.sendFile(__dirname + '/style.css');
+app.get("/style.css", (req, res) => {
+	res.sendFile(`${__dirname}/style.css`);
 });
 
-io.on('connection', (socket) => {
-    console.log('a user connected');
-    socket.on('disconnect', () => {
-        console.log('user disconnected');
-    });
+io.on("connection", (socket) => {
+	console.log("a user connected");
+	socket.on("disconnect", () => {
+		console.log("user disconnected");
+	});
 });
 
-io.on('connection', (socket) => {
-    socket.on('chat message', (msg) => {
-        msg.dateTime = new Date();
-        io.emit('chat message', msg);
-    });
-});
-
-http.listen(process.env.PORT || 8080, () => {
-    console.log('listening on *:3000');
+io.on("connection", (socket) => {
+	socket.on("chat message", (msg) => {
+		msg.dateTime = new Date();
+		io.emit("chat message", msg);
+	});
 });
