521bf2c645701e0b241043a08d4385e563124867
Andrei <azayats@xbsoftware.by>
2020-05-19 11:39:37 +0300

    fix port

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-const app = require("express")();
+let express = require('express');
+let app = express();
 app.use(express.static(__dirname));
 const port = process.env.PORT || 3000;
 const server = app.listen(port, () => {
