47f22dfb60628fca79f81592f23188a5365e959d
Andrei <azayats@xbsoftware.by>
2020-06-10 18:12:02 +0300

    fix

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 app.use(express.static(__dirname));
-app.use(express.static(path.join(__dirname, "/app/"));
+app.use(express.static(path.join(__dirname, "/app/")));
 
 const server = app.listen(port, () => {
 	console.log(`Server listening on port: ${port}`);
